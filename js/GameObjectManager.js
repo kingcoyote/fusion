e51e48fb76203d87fd5fdeac8189f0966da5e6da
Steve <visual77@gmail.com>
2012-02-06 13:49:56 -0800

    removed Utils.js and moved the array.remove and array.removeObject functions just to the one array that uses it, so it won't screw up every other array in the system.

--- js/GameObjectManager.js
+++ js/GameObjectManager.js
@@ -9,6 +9,33 @@ function GameObjectManager()
         @type Arary
    */
   this.gameObjects = new Array();
+  /**
+    Removes a number of objects from the array
+    @param from The first object to remove
+    @param to (Optional) The last object to remove
+  */
+  this.gameObjects.remove = function(/**Number*/ from, /**Number*/ to)
+  {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+  };
+  /**
+    Removes a specific object from the array
+    @param object The object to remove
+  */
+  this.gameObjects.removeObject = function(object)
+  {
+    for (var i = 0; i < this.length; ++i)
+    {
+        if (this[i] === object)
+        {
+            this.remove(i);
+            break;
+        }
+    }
+  }
+  
   /** The time that the last frame was rendered  
         @type Date
    */
