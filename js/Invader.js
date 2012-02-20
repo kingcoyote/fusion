cf60bcd811a23722b07fb5a791dc84bffd21b0c9
Steve <visual77@gmail.com>
2012-02-20 13:30:18 -0800

    added support for multiple guns, modified invader3 to have dual guns

--- js/Invader.js
+++ js/Invader.js
@@ -1,10 +1,10 @@
 function Invader() {
   this.invaders = {
-    invader1 : { image : g_ResourceManager.invader1, width: 35, height: 45, gun : { x : 17, y : 41 }, cooldown: 2.5, health: 10 },
-    invader2 : { image : g_ResourceManager.invader2, width: 50, height: 49, gun : { x : 25, y : 44 }, cooldown: 2.5, health: 10 },
-    invader3 : { image : g_ResourceManager.invader3, width: 54, height: 89, gun : { x : 27, y : 84 }, cooldown: 2.5, health: 20 },
-	invader4 : { image : g_ResourceManager.invader4, width: 120, height: 106, gun : { x : 59, y : 105 }, cooldown: 2.5, health: 50 },
-	invader5 : { image : g_ResourceManager.invader5, width: 75, height: 72, gun : { x : 37, y : 52 }, cooldown: 2.5, health: 30 }
+    invader1 : { image : g_ResourceManager.invader1, width: 35, height: 45, gun : [{ x : 17, y : 41 }], cooldown: 2.5, health: 10 },
+    invader2 : { image : g_ResourceManager.invader2, width: 50, height: 49, gun : [{ x : 25, y : 44 }], cooldown: 2.5, health: 10 },
+    invader3 : { image : g_ResourceManager.invader3, width: 54, height: 89, gun : [{ x : 11, y : 40 }, { x : 43, y : 40}], cooldown: 2.5, health: 20 },
+    invader4 : { image : g_ResourceManager.invader4, width: 120, height: 106, gun : [{ x : 59, y : 105 }], cooldown: 2.5, health: 50 },
+    invader5 : { image : g_ResourceManager.invader5, width: 75, height: 72, gun : [{ x : 37, y : 52 }], cooldown: 2.5, health: 30 }
   };
   
   this.points = 10;
@@ -35,8 +35,11 @@ function Invader() {
   };
   
   this.shoot = function() {
-    new Bullet().startupBullet(this.x + this.type.gun.x, this.y + this.type.gun.y, 1);
-    new TempVisualGameObject().startupTempVisualGameObject(g_ResourceManager.flashDown, this.x + this.type.gun.x - 27 , this.y + this.type.gun.y, 5, 0.05);
+    for(var i in this.type.gun) {
+      gun = this.type.gun[i];
+      new Bullet().startupBullet(this.x + gun.x, this.y + gun.y, 1);
+      new TempVisualGameObject().startupTempVisualGameObject(g_ResourceManager.flashDown, this.x + gun.x - 27 , this.y + gun.y, 5, 0.05);
+    }
 
     this.cooldown = this.type.cooldown + (Math.random() * this.type.cooldown);
   };
