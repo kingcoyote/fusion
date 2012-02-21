e34008244cf0ae5ad4a745c0ceb16656cc6312f6
Steve <kingcoyote85@gmail.com>
2012-02-21 13:54:44 -0800

    changed how invader points work to allow different invaders to have different points. also added a wave after the boss so it doesnt loop on the boss

--- js/Invader.js
+++ js/Invader.js
@@ -1,16 +1,15 @@
 function Invader() {
   this.invaders = {
-    invader1 : { image : g_ResourceManager.invader1, width: 35, height: 45, gun : [{ x : 17, y : 41 }], cooldown: 2.5, health: 10 },
-    invader2 : { image : g_ResourceManager.invader2, width: 50, height: 49, gun : [{ x : 25, y : 44 }], cooldown: 2.5, health: 10 },
-    invader3 : { image : g_ResourceManager.invader3, width: 54, height: 89, gun : [{ x : 11, y : 40 }, { x : 43, y : 40}], cooldown: 2.5, health: 20 },
-    invader4 : { image : g_ResourceManager.invader4, width: 120, height: 106, gun : [{ x : 18, y : 57 }, { x : 100, y : 57}, { x : 59, y : 105}], cooldown: 2, health: 100 },
-    invader5 : { image : g_ResourceManager.invader5, width: 75, height: 72, gun : [{ x : 37, y : 52 }], cooldown: 1, health: 30 },
+    invader1 : { image : g_ResourceManager.invader1, width: 35, height: 45, gun : [{ x : 17, y : 41 }], cooldown: 2.5, health: 10, points : 10 },
+    invader2 : { image : g_ResourceManager.invader2, width: 50, height: 49, gun : [{ x : 25, y : 44 }], cooldown: 2.5, health: 10, points : 15 },
+    invader3 : { image : g_ResourceManager.invader3, width: 54, height: 89, gun : [{ x : 11, y : 40 }, { x : 43, y : 40}], cooldown: 2.5, health: 20, points:25 },
+    invader4 : { image : g_ResourceManager.invader4, width: 120, height: 106, gun : [{ x : 18, y : 57 }, { x : 100, y : 57}, { x : 59, y : 105}], cooldown: 2, health: 100, points:50 },
+    invader5 : { image : g_ResourceManager.invader5, width: 75, height: 72, gun : [{ x : 37, y : 52 }], cooldown: 1, health: 30, points: 35 },
     invader6 : { image : g_ResourceManager.bossWingLeft, width: 214, height: 281, gun : [{ x : 86, y : 280}], cooldown: 0.75, health: 200 },
     invader7 : { image : g_ResourceManager.bossWingRight, width: 214, height: 281, gun : [{ x : 127, y : 280}], cooldown: 0.75, health: 200 },
-    invader8 : { image: g_ResourceManager.bossBody, width: 184, height: 234, gun : [{ x : 16, y : 223}, { x : 92, y : 186}, { x : 167, y : 223}], cooldown: 0.5, health: 250 }
+    invader8 : { image : g_ResourceManager.bossBody, width: 184, height: 234, gun : [{ x : 16, y : 223}, { x : 92, y : 186}, { x : 167, y : 223}], cooldown: 0.5, health: 250, points:300 }
   };
   
-  this.points = 10;
   this.controller = false;
   this.team = 1;
   this.original_y = 0;
@@ -23,6 +22,7 @@ function Invader() {
     this.controller = controller;
     this.type = this.invaders['invader' + type];
     this.health = this.type.health;
+    this.points = this.type.points;
     this.cooldown = Math.random() * this.type.cooldown;
     this.original_y = y;
     this.startupVisualGameObject(this.invaders['invader' + type].image, x, y, 1);
