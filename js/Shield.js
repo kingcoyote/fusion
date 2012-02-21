feb7787f08db138d37cf70c31bf27b23d88e80cc
Steve <kingcoyote85@gmail.com>
2012-02-21 13:54:59 -0800

    created an upgrade to enhance shield health

--- js/Shield.js
+++ js/Shield.js
@@ -1,9 +1,8 @@
 Shield = function() {
-  var self = this;
   this.team = 0;
   this.destructible = true;
   this.points = 0;
-  this.health = 1;
+  this.health = Shield.stats.health;
   this.shield = true;
   this.startupShield = function(x, y) {
     this.startupVisualGameObject(g_ResourceManager.shield, x, y, 1);
@@ -20,6 +19,16 @@ Shield.shieldRestore = function() {
   g_ApplicationManager.startShields();
 };
 
+Shield.shieldHealth = function() {
+  Shield.stats.health += 10;
+  g_ApplicationManager.startShields();
+};
+
 Shield.StoreInventory = [
-  /* shield restore */   { name: "Shield Restore", icon : "repairshields", cost: "150", callback: Shield.shieldRestore }
+  /* shield restore */   { name: "Shield Restore", icon : "shieldrestore", cost: "150", callback: Shield.shieldRestore },
+  /* shield health */ { name : "Increase Health", icon : "shieldhealth", cost:"150", callback: Shield.shieldHealth }
 ];
+
+Shield.stats = {
+  health : 10
+};
