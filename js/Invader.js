03f6bed8499e555e90c172d75317de93bba0d083
Steve <visual77@gmail.com>
2012-03-04 11:10:20 -0800

    added more waves, gave speeds to the remaining invaders, and stopped the infinite loop

--- js/Invader.js
+++ js/Invader.js
@@ -95,9 +95,9 @@ Invader.prototype.setDirection = function(x, y) {
 Invader.invaders = {
   invader1 : { image : 'invader1', width: 35, height: 45, gun : [{ x : 17, y : 41 }], cooldown: 1.5, health: 10, points : 10, z:1, speed:100 },
   invader2 : { image : 'invader2', width: 50, height: 49, gun : [{ x : 25, y : 44 }], cooldown: 0.25, health: 30, points : 30, z:1, speed : 50 },
-  invader3 : { image : 'invader3', width: 54, height: 89, gun : [{ x : 11, y : 40 }, { x : 43, y : 40}], cooldown: 1.5, health: 50, points:65, z:1 },
-  invader4 : { image : 'invader4', width: 120, height: 106, gun : [{ x : 18, y : 57 }, { x : 100, y : 57}, { x : 59, y : 105}], cooldown: 1, health: 150, points:200, z:1 },
-  invader5 : { image : 'invader5', width: 75, height: 72, gun : [{ x : 37, y : 52 }], cooldown: 0.5, health: 80, points: 100, z:1 },
+  invader3 : { image : 'invader3', width: 54, height: 89, gun : [{ x : 11, y : 40 }, { x : 43, y : 40}], cooldown: 1.5, health: 50, points:65, z:1, speed: 75 },
+  invader4 : { image : 'invader4', width: 120, height: 106, gun : [{ x : 18, y : 57 }, { x : 100, y : 57}, { x : 59, y : 105}], cooldown: 1, health: 150, points:200, z:1, speed:100 },
+  invader5 : { image : 'invader5', width: 75, height: 72, gun : [{ x : 37, y : 52 }], cooldown: 0.5, health: 80, points: 100, z:1, speed:75 },
   invader6 : { image : 'bossWingLeft', width: 214, height: 281, gun : [{ x : 86, y : 280}], cooldown: 0.75, health: 200, points:0, z:2 },
   invader7 : { image : 'bossWingRight', width: 214, height: 281, gun : [{ x : 127, y : 280}], cooldown: 0.75, health: 200, points:0, z:2 },
   invader8 : { image : 'bossBody', width: 184, height: 234, gun : [{ x : 16, y : 223}, { x : 92, y : 186}, { x : 167, y : 223}], cooldown: 0.5, health: 250, points:0, z:1 }
@@ -162,7 +162,7 @@ InvaderController.prototype.update = function(dt) {
     g_level++;
     g_ApplicationManager.updateLevel();
     if(! InvaderWaves[g_level]) {
-      //g_ApplicationManager.gameOver('Congratulations for beating game!');
+      g_ApplicationManager.gameOver('Congratulations for beating game!');
     }
     setTimeout(function(){
       Store.showStore();
@@ -181,5 +181,12 @@ InvaderController.prototype.getRandomCoords = function() {
 }
 
 var InvaderWaves = [ null, 
-   { duration : 10, invaders : [1,1,2,1,1,1,1,1] },
+   { duration : 10, invaders : [1,1,2,1,1,2,1,1] },
+   { duration : 10, invaders : [1,1,2,2,1,1,2,2,1,1,2,2,3] },
+   { duration : 10, invaders : [2,2,2,3,2,3,2,2] },
+   { duration : 10, invaders : [1,1,2,3,1,1,1,1,2,3,1,1,1,1,2,3] },
+   { duration : 10, invaders : [2,2,2,3,3,2,2,5,2,2] },
+   { duration : 10, invaders : [2,3,3,2,3,3,5,3,5] },
+   { duration : 10, invaders : [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,4] },
+   { duration : 10, invaders : [2,2,3,2,3,3,2,5,5] },
  ];
