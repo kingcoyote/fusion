274f0cc4de398c0692f95d8e11eba4c1c75a6061
Steve <visual77@gmail.com>
2012-03-04 10:12:47 -0800

    fixed up the turret rotation and firing

--- js/Turret.js
+++ js/Turret.js
@@ -1,4 +1,8 @@
 function Turret(x,y) {
+  this.firespeed = 0.75;
+  this.cooldown  = 0;
+  this.angle = 0;
+  
   this.image = g_ResourceManager.turret;
   
   VisualGameObject.call(
@@ -9,7 +13,7 @@ function Turret(x,y) {
     1
   );
   this.sprite.initFrames(7,4);
-  this.sprite.setFrame(0);
+  this.sprite.setFrame(0,0);
   
   this.x -= this.sprite.width / 2;
   this.y -= this.sprite.height / 2;
@@ -21,7 +25,7 @@ function Turret(x,y) {
     2
   );
   this.gun.sprite.initFrames(7,4);
-  this.gun.sprite.setFrame(1,1);
+  this.gun.sprite.setFrame(0,1);
   g_GameObjectManager.addGameObject(this.gun);
   
   // placeholder target to force refresh on first update
@@ -33,23 +37,54 @@ function Turret(x,y) {
 Turret.prototype = new VisualGameObject();
 
 Turret.prototype.update = function(dt) {
-  if(this.target.dead) {
-    this.locateTarget();
+  this.locateTarget();
+  if(this.target.x && this.target.y) {
+    this.setDirection(Math.atan2(this.y - this.target.y, this.x - this.target.x) - Math.PI / 2);
+  } else {
+    this.setDirection(0);
+  }
+  
+  this.cooldown -= dt;
+  
+  if(this.cooldown <= 0 && ! this.target.dead) {
+    this.shoot();
+    this.cooldown = this.firespeed;
   }
 };
 
 Turret.prototype.locateTarget = function() {
-  for(var i in g_ApplicationManager.invaderController) {
-    var invader = g_ApplicationManager.invaderController[i];
+  var distance = Infinity,
+    target = { dead : true };
+  for(var i in g_ApplicationManager.invaderController.invaders) {
+    var invader = g_ApplicationManager.invaderController.invaders[i],
+      x = invader.x,
+      y = invader.y,
+      d = Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+    
+    if(invader.dead) continue;
+    
+    if(d < distance) {
+      target = invader;
+      distance = d;
+    }
   }
+  
+  this.target = target;
 };
+
+Turret.prototype.shoot = function() {
+  var bullet = new Bullet(
+    this.x + this.sprite.width / 2,
+    this.y + this.sprite.height / 2,
+    this.angle,
+    -1
+  );
+  
+  g_GameObjectManager.addGameObject(bullet);
+}
 
 Turret.prototype.setDirection = function(angle) {
   this.angle = angle;
   this.sprite.rotate(angle);
   this.gun.sprite.rotate(angle);
-};
-
-Turret.prototype.shoot = function() {
-  
 };
